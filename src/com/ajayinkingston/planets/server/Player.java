1074ef16f230eeeaccdb68e1a65e1deae69593ee
thebestusernameever <ajay@ajayinkingston.com>
2017-11-16 07:59:55 -0500

    uncommnted the movement and the client still moves, has to be the issue

--- src/com/ajayinkingston/planets/server/Player.java
+++ src/com/ajayinkingston/planets/server/Player.java
@@ -126,26 +126,26 @@ public class Player extends Entity{
 		yspeed += gravityy*delta;
 		
 		//movement
-		if(right){
-			xspeed += Math.cos(Main.getClosestAngle(this, data.planets)+1.5708)*data.speed * delta;//1.5708 is 90 degrees in radians (half pi or quarter tau)
-			yspeed += Math.sin(Main.getClosestAngle(this, data.planets)+1.5708)*data.speed * delta;//1.5708 is 90 degrees in radians (half pi or quarter tau)
-			
-//			System.out.println(frames + " MOVED BY X " + (Math.cos(main.getClosestAngle(this)+1.5708)*main.speed * delta) + " MOVED BY Y " + Math.sin(main.getClosestAngle(this)+1.5708)*main.speed * delta + " AT ANGLE " + main.getClosestAngle(this) + " X " + x + " Y " + y);
-			
-//			x+=Math.cos(0) * 500*delta;
-//			y+=Math.sin(0) * 500*delta;
-			
-//			if(rightstart == -1){
-//				rightstart = frames;
-//			}
-		}
-		if(left){
-			xspeed += Math.cos(Main.getClosestAngle(this, data.planets)-1.5708)*data.speed * delta;//1.5708 is 90 degrees in radians (half pi or quarter tau)
-			yspeed += Math.sin(Main.getClosestAngle(this, data.planets)-1.5708)*data.speed * delta;//1.5708 is 90 degrees in radians (half pi or quarter tau)
-
-//			x+=Math.cos(-Math.PI) * 500*delta;
-//			y+=Math.sin(-Math.PI) * 500*delta;
-		}
+//		if(right){
+//			xspeed += Math.cos(Main.getClosestAngle(this, data.planets)+1.5708)*data.speed * delta;//1.5708 is 90 degrees in radians (half pi or quarter tau)
+//			yspeed += Math.sin(Main.getClosestAngle(this, data.planets)+1.5708)*data.speed * delta;//1.5708 is 90 degrees in radians (half pi or quarter tau)
+//			
+////			System.out.println(frames + " MOVED BY X " + (Math.cos(main.getClosestAngle(this)+1.5708)*main.speed * delta) + " MOVED BY Y " + Math.sin(main.getClosestAngle(this)+1.5708)*main.speed * delta + " AT ANGLE " + main.getClosestAngle(this) + " X " + x + " Y " + y);
+//			
+////			x+=Math.cos(0) * 500*delta;
+////			y+=Math.sin(0) * 500*delta;
+//			
+////			if(rightstart == -1){
+////				rightstart = frames;
+////			}
+//		}
+//		if(left){
+//			xspeed += Math.cos(Main.getClosestAngle(this, data.planets)-1.5708)*data.speed * delta;//1.5708 is 90 degrees in radians (half pi or quarter tau)
+//			yspeed += Math.sin(Main.getClosestAngle(this, data.planets)-1.5708)*data.speed * delta;//1.5708 is 90 degrees in radians (half pi or quarter tau)
+//
+////			x+=Math.cos(-Math.PI) * 500*delta;
+////			y+=Math.sin(-Math.PI) * 500*delta;
+//		}
 		
 		//friction
 //		if(Math.abs(xspeed) < friction*delta) xspeed = 0;
