2f460607caa7807150286dc10f40182eed650508
thebestusernameever <ajay@ajayinkingston.com>
2017-11-19 22:00:57 -0500

    Has to be an issue with gravty or movement or something because now even with tests it goes off.

--- src/com/ajayinkingston/planets/server/Main.java
+++ src/com/ajayinkingston/planets/server/Main.java
@@ -292,13 +292,13 @@ public class Main extends Canvas implements ClientMessageReceiver, Runnable{
 			return false; //returns false to be called later when this movement actually happens
 		}
 		
-		if(direction){
-			player.right = !disable;
-		}else{
-			player.left = !disable;
-		}
-//		player.move(data, currentFrame);
-		if(true) return true;
+//		if(direction){
+//			player.right = !disable;
+//		}else{
+//			player.left = !disable;
+//		}
+////		player.move(data, currentFrame);
+//		if(true) return true;
 		
 		OldState originalState = getOldStateAtFrame(new ArrayList<>(player.oldStates), frame);
 		if(originalState == null){
