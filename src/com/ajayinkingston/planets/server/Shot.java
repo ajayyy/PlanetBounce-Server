f0528f690d7baa9c50c83a63596ca196e83d18e5
thebestusernameever <ajay@ajayinkingston.com>
2017-11-21 22:00:39 -0500

    Made variable public

--- src/com/ajayinkingston/planets/server/Shot.java
+++ src/com/ajayinkingston/planets/server/Shot.java
@@ -4,7 +4,7 @@ public class Shot {
 	public long frame;
 	public float projectileangle;
 	
-	Player player;
+	public Player player;
 	public Shot(Player player, float projectileangle, long frame){
 		this.player = player;
 		this.frame = frame;
